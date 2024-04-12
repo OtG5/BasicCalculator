ad835c6465d847d897a7e578fa03ee5ca23f9b9b
OtG5 <liloscar101@gmail.com>
2024-04-12 10:56:56 -0400

    completed Mod 3

---
+++ src/main/java/BasicCalculator/calculator.java
@@ -0,0 +1,11 @@
+package BasicCalculator;
+
+public class calculator {
+    public static void main(String[] args) {
+
+        int firstNUm = 5;
+        int secondNum = 12;
+
+        System.out.println(firstNUm + secondNum);
+    }
+}
