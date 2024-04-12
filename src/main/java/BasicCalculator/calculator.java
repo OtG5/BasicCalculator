749e5335504b700af5b02520f5a99e425bde3637
OtG5 <liloscar101@gmail.com>
2024-04-12 13:07:57 -0400

    fixed my code

--- src/main/java/BasicCalculator/calculator.java
+++ src/main/java/BasicCalculator/calculator.java
@@ -1,11 +1,28 @@
 package BasicCalculator;
-
+import java.util.Scanner;
 public class calculator {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        float num1 = scanner.nextFloat();
 
-        int firstNUm = 5;
-        int secondNum = 12;
+        System.out.print("Enter the second number: ");
+            float num2 = scanner.nextFloat();
 
-        System.out.println(firstNUm + secondNum);
+        System.out.println("Possible calculations: ");
+        System.out.println("(A)dd");
+        System.out.println("(S)ubtract");
+        System.out.println("(M)ultiply");
+        System.out.println("(D)ivide");
+
+        scanner.nextLine();
+
+        System.out.print("Please select an option: ");
+        String option = scanner.nextLine();
+
+
+        System.out.println("");
+
+
     }
 }
